57cc9189cfb3ff632a27c1291f752bd7da87c5ff
Victor <93vitinho2012@gmail.com>
2023-02-17 09:19:54 -0500

    set up input validation for inputs.

--- main.py
+++ main.py
@@ -17,14 +17,25 @@ unique (perhaps not necessary to code, user would be responsible for this part)
 
 while True:
     try:
-        pass_min_len = int(input("Input password length (minimum of 6):"))
+        pass_min_len = int(input("Input password length (minimum of 6, recommendation of 12 or more):"))
         if pass_min_len < 6:
             raise ValueError("Password length must be at least 6.")
         break
     except ValueError as e:
         print("Error:", e, "Please try again.")
 
-spc_char_need = input("Can you have special characters in your password?(yes or no only):")
+# spc_char_need = input("Can you have special characters in your password?(yes or no only):")
+while True:
+    try:
+        spc_char_need = input("Can you have special characters in your password?(yes or no only):")
+        spc_char_need = spc_char_need.lower()
+        if spc_char_need != "yes" and spc_char_need != "no":
+            raise ValueError("Must be a yes or no answer.")
+        break
+    except ValueError as e:
+        print("Error:", e, "Please try again.")
+
+
 
 # character list for possible characters. seperated special characters and punctuation for better readability.
 possible_characters = string.ascii_letters + string.digits
